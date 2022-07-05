59c18536e33928cc82ddac43cfe62a4c8afec8b8
Hao Phan <nguyenhao.phan@gmail.com>
2022-07-05 23:42:45 +0300

    add google strategy

---
+++ api/src/auth/google-strategy/google.strategy.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { Strategy, VerifyCallback } from 'passport-google-oauth20'
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(protected configService: ConfigService) {
+    super({
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
+      callbackURL: 'http://localhost:3000/oauth2/redirect/google',
+      scope: ['profile'],
+    })
+  }
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    dont: VerifyCallback,
+  ) {
+    return
+  }
+}
