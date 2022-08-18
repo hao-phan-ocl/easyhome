479b7f901eae0d2f49c7baa5f3a41cd01529703e
Hao Phan <nguyenhao.phan@gmail.com>
2022-08-19 01:34:19 +0300

    update

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(5000)
+  await app.listen(process.env.PORT || 5000)
 }
 bootstrap()
