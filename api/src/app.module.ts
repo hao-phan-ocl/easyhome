06bfa10d6d133312e7fc49dfb248f44f9cd80fe5
Hao Phan <nguyenhao.phan@gmail.com>
2022-08-18 23:21:04 +0300

    update environment variables

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,16 +1,29 @@
 import { Module } from '@nestjs/common'
 import { MongooseModule } from '@nestjs/mongoose'
-import { ConfigModule } from '@nestjs/config'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MulterModule } from '@nestjs/platform-express'
 
 import { RoomsModule } from './rooms/rooms.module'
 import { AuthModule } from './auth/auth.module'
 import { UsersModule } from './users/users.module'
 
+const ENV = process.env.NODE_ENV
+
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/easyhome'),
-    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    // MongooseModule.forRoot('mongodb://localhost:27017/easyhome'),
+    // ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    MongooseModule.forRootAsync({
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true,
+      cache: true,
+    }),
     MulterModule.register({ dest: './upload' }),
     RoomsModule,
     AuthModule,
--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import {
   Res,
   UseGuards,
 } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
 import { Response } from 'express'
 
 import { AuthService } from './auth.service'
@@ -15,7 +16,10 @@ import { LocalAuthGuard } from './local-strategy/local-auth.guard'
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly configService: ConfigService,
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('login/local')
@@ -34,9 +38,10 @@ export class AuthController {
   async googleAuthRedirect(@Req() req: any, @Res() res: Response) {
     const jwt = await this.authService.googleLogin(req.user)
 
-    if (jwt)
-      res.redirect('http://localhost:3000/login/success/' + jwt.access_token)
-    else res.redirect('http://localhost:3000/login/failed')
+    const hostURL = this.configService.get<string>('HOST_URL')
+
+    if (jwt) res.redirect(`${hostURL}/login/success/` + jwt.access_token)
+    else res.redirect(`${hostURL}/login/failed`)
 
     return jwt
   }
