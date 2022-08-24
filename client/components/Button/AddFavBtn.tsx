06ea8e22b300c36ab85dabace64c7715059a4761
Hao Phan <nguyenhao.phan@gmail.com>
2022-08-24 04:56:59 +0300

    update add fav

--- client/components/Button/AddFavBtn.tsx
+++ client/components/Button/AddFavBtn.tsx
@@ -1,6 +1,7 @@
 import { IconButton, Tooltip } from '@mui/material'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
+import { useRouter } from 'next/router'
 
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks'
 import { addFav, removeFav } from '../../redux/features/authSlice'
@@ -11,13 +12,16 @@ type Props = {
 
 export default function AddFavBtn({ roomId }: Props) {
   const dispatch = useAppDispatch()
+  const router = useRouter()
   const { user } = useAppSelector((state) => state.auth)
 
   const added = user?.favLists.some((elem) => elem._id === roomId)
 
   function handleOnClick(roomId: string) {
-    if (added) dispatch(removeFav(roomId))
-    else dispatch(addFav(roomId))
+    if (user) {
+      if (added) dispatch(removeFav(roomId))
+      else dispatch(addFav(roomId))
+    } else router.push('/login')
   }
 
   return (
